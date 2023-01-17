498d43c5e7cdd6fddbe720bb2d91c20d03c807d6
Eundding <104212013+Eundding@users.noreply.github.com>
2023-01-18 00:39:50 +0900

    Update 20044_이은정.py

--- 1. 그리디/20044번 Project Teams/20044_이은정.py
+++ 1. 그리디/20044번 Project Teams/20044_이은정.py
@@ -1,1 +1,17 @@
+# Project Teams
+# 1 2 3 4 5 6 이면 양끝끼리 더해서 구한다
 
+import sys
+input = sys.stdin.readline
+
+N = int(input()) # 팀 수
+ability = list(map(int, input().split())) # 학생 코딩 역량 리스트로 넣음
+
+ability.sort() # 코딩 역량 오름차순 정렬
+
+min = 200000 # 코딩 역량 최솟값
+for i in range(N):
+    if ability[i] + ability[2*N - i - 1] < min: # 최솟값보다 작으면 최솟값으로 정의
+        min = ability[i] +  ability[2*N - i - 1]
+
+print(min)
