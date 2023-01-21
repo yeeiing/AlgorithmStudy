83eb78f36b035efd713b219cc777675443b68b49
Taehyun An <ateahyun@naver.com>
2023-01-22 03:29:54 +0900

    안태현

--- 1. 그리디/2885번 초콜릿 식사/2885_안태현.py
+++ 1. 그리디/2885번 초콜릿 식사/2885_안태현.py
@@ -5,15 +5,15 @@ input = sys.stdin.readline
 k = int(input())
 
 d = 2
-while d < k:
+while d < k: # 최소 필요한 초콜릿 정사각형 개수 구함
   d *= 2
 
 D = d
 cnt = 0
 while k > 0:
-  if k >= d:
+  if k >= d: # k가 더 크다면 k에서 빼줌
     k -= d
   else:
-    d = d // 2
+    d = d // 2 # 반으로 쪼개기
     cnt += 1
 print(D, cnt)
