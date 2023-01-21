5cc98c9f3b5ab17b769ddb96fbaa9569c9443511
Taehyun An <ateahyun@naver.com>
2023-01-22 03:16:45 +0900

    안태현

--- 1. 그리디/12018번 Yonsei TOTO/12018_안태현.py
+++ 1. 그리디/12018번 Yonsei TOTO/12018_안태현.py
@@ -4,7 +4,7 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-sub_m = [] # 필요한 마일리지 배열
+sub_m = [] # 수강 신청에 필요한 마일리지 배열
 ans = 0
 for i in range(n):
   p, l = map(int, input().split())
@@ -15,9 +15,9 @@ for i in range(n):
       ans += 1
   elif p >= l: # 수강인원보다 신청한 인원이 많다면
     arr.sort(reverse=1) # 내림차순으로 정렬
-    sub_m.append(arr[l - 1])
+    sub_m.append(arr[l - 1]) # 최소한의 마일리지로 수강인원에 들어야하므로 l번째 만큼 마일리지 분배
 
-sub_m.sort() # 오름차순으로 정렬
+sub_m.sort() # 오름차순으로 정렬 | 필요한 마일리지가 적은 과목부터 분배하기 위해
 for item in sub_m:
   if item <= m:
     m -= item
