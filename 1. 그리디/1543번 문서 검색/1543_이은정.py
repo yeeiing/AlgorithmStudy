ab27fa80c165b40594da4dcaa3f549c4304c7edb
EunJeong Lee <104212013+Eundding@users.noreply.github.com>
2023-02-02 23:00:48 +0900

    이은정

--- 1. 그리디/1543번 문서 검색/1543_이은정.py
+++ 1. 그리디/1543번 문서 검색/1543_이은정.py
@@ -1,1 +1,7 @@
+import sys
+input = sys.stdin.readline
 
+question = input().split('\n') #'\n'을 기준으로 잘랐으므로 주어진 문장은 question[0]
+search = input().split('\n') #'\n'을 기준으로 잘랐으므로 주어진 문장은 search[0]
+
+print(question[0].count(search[0])) # 개수세기
