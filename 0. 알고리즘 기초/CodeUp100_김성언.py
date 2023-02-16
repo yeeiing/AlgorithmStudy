9484219ee2052b3734de0e8a97c3d65ea82adc0b
E ON <ykiki5778@inha.edu>
2023-02-16 11:47:28 +0900

    김성언

--- 0. 알고리즘 기초/CodeUp100_김성언.py
+++ 0. 알고리즘 기초/CodeUp100_김성언.py
@@ -98,3 +98,80 @@ print(n[0:2],n[2:4],n[4:6])
 # print(n[3:5])
 hour, minute, second = input().split(":")
 print(minute)
+
+
+# 6024
+a, b =input().split()
+print(a+b)
+
+# 6025 
+n = list(map(int, input().split()))
+print(n[0] + n[1])
+
+# 6026 
+a = float(input())
+b = float(input())
+print(a+b)
+
+# 6027
+# https://velog.io/@robinyeon/python-16진수로-출력하기
+n = int(input())
+print("%x"%n)
+
+# 6028
+# print('%X' % n)  #n에 저장되어있는 값을 16진수 대문자 형태 문자열로 출력
+n = int(input())
+print("%X"%n)
+
+# 6029
+# n = hex(int(input()))
+a = input() 
+n = int(a, 16)      #입력된 a를 16진수로 인식해 변수 n에 저장 
+print('%o' % n)  #n에 저장되어있는 값을 8진수(octal) 형태 문자열로 출력 
+
+# 6030
+n = ord(input())
+print(n)
+
+# 6031
+c = int(input())
+print(chr(c)) 
+
+# 6032
+n = int(input())
+print(-n)
+# 단항(unary) 연산자인 -(negative)를 변수 앞에 붙이면 부호가 반대인 값이 된다. 
+
+# 6033
+n = ord(input())
+print(chr(n+1))
+
+# 6034
+n, m = input().split()
+print( int(n) - int(m))
+
+# 6035
+n, m = input().split()
+print( float(n) * float(m))
+
+# 6036
+n, m = input().split()
+print(n*int(m))
+
+# 6037
+n = int(input())
+m = input()
+print(n * m)
+
+# 6038
+n, m = input().split()
+print(int(n)**int(m))
+
+# 6039
+n, m = input().split()
+print(float(n)**float(m))
+
+# 6040
+n, m = input().split()
+print(int(n)//int(m))
+
